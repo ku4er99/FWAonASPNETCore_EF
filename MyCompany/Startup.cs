4c949e065a3cbb260e401f94046472a525bd6261
Ku4eR <Ku4eR99@gmail.com>
2021-01-26 11:47:14 +0300

    step 2. Через NuGet подключены все нужные пакеты. Через сервисы добавлена поддержка контроллеров, представлений, статических файлов(стилей, скриптов и тд). Зарегистрирован собственный маршрут на дефолтной странице.

--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -7,33 +7,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyCompany
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews() // Добавляем поддержку контроллеров и представлений (MVC)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider(); // Выставляем совместимость с asp.net core 3.0 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            ///
+            /// ОЧЕНЬ ВАЖЕН ПОРЯДОК РЕГИСТРАЦИИ MIDDLEWARE (функционала)
+            /// 
+
+            if (env.IsDevelopment()) // при окружении == Development
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseRouting(); // Подключение системы маршрутизации
 
-            app.UseEndpoints(endpoints =>
+            app.UseStaticFiles(); // Подключаем поддержку статичных файлов (css, js, etc..)
+
+            app.UseEndpoints(endpoints => // Регистрируем нужные нам маршруты (ендпоинты)
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}"); // Условно: При переходе на site.com/ система маршрутизации будет использовать контроллер HOME, действие INDEX
             });
         }
     }
